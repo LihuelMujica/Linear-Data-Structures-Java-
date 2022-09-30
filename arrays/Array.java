bfbbe8d98bffe4ec22fcaa4f553ef21154b309da
LihuelMujica <lihuelmujica@gmail.com>
2022-09-30 17:09:52 -0300

    Do some refactor and extra exercise

--- arrays/Array.java
+++ arrays/Array.java
@@ -1,9 +1,5 @@
 package arrays;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 public class Array{
      int[] values;
      int count=0;
@@ -19,25 +15,22 @@ public class Array{
     private void resize(int length){
         int[] copy = values.clone();
         values = new int[length];
-        for (int i = 0; i < copy.length; i++) {
-            values[i] = copy[i];
-        }
+        System.arraycopy(copy, 0, values, 0, copy.length);
     }
 
     public void insert(int value) {
         if(count==values.length) resize(values.length*2);
-        values[count] = value;
-        count++;
+        values[count++] = value;
     }
 
     public void removeAt(int pos) {
-        int[] copy = values.clone();
-        values = new int[values.length];
-        for (int i = 0; i <count; i++) {
-            if(i==pos) continue;
-            values[i] = copy[i];
-        }
-        count--;
+        if (pos < 0 || pos >= count)
+            throw new IllegalArgumentException();
+
+        for (int i = pos; i < count; i++)
+            values[i] = values[i+1];
+
+        count --;
     }
 
     public int indexOf(int value) {
@@ -47,12 +40,49 @@ public class Array{
         return -1;
     }
 
+    public int max() {
+        if(count<=0) return -1;
+        int max = values[0];
+        for (int value: values) {
+            if (value > max) max = value;
+        }
+        return max;
+    }
+
+    public Array intersect(Array other) {
+        Array intersection = new Array(count);
+
+        for (int i = 0; i < count; i++) {
+            for (int value: other.values) {
+                if(value == values[i]) intersection.insert(value);
+            }
+        }
+
+        return intersection;
+    }
+
+    public void reverse(){
+        for (int i = 0; i < count/2; i++) {
+            int copy = values[i];
+            values[i] = values[count-1-i];
+            values[count-1-i] = copy;
+        }
+    }
+
+    public void insertAt(int item, int index){
+        for (int i = count-1; i >= index; i--) {
+            values[i+1] = values[i];
+        }
+        values[index] = item;
+        count++;
+    }
+
     @Override
     public String toString() {
-        String out = "[";
+        StringBuilder out = new StringBuilder("[");
         for (int i = 0; i < count; i++) {
-            out += values[i];
-            if(i<count-1) out+=",";
+            out.append(values[i]);
+            if(i<count-1) out.append(",");
         }
         return out + "]";
     }
--- arrays/ArrayTest.java
+++ arrays/ArrayTest.java
@@ -12,17 +12,31 @@ class ArrayTest {
     @BeforeEach
     public void before() {
         array = new Array(3);
-    }
-
-    @Test
-    public void test(){
         array.insert(10);
         array.insert(20);
         array.insert(30);
         array.insert(40);
+    }
+
+    @Test
+    public void test(){
         array.removeAt(3);
         assertEquals(0,array.indexOf(10));
         assertEquals("[10,20,30]",array.toString());
+    }
+
+    @Test
+    public void ex() {
+        var array2 = new Array(2);
+        array2.insert(10);
+        array2.insert(40);
+        assertEquals(40,array.max());
+        assertEquals("[10,40]",array.intersect(array2).toString());
+        array.reverse();
+        assertEquals("[40,30,20,10]",array.toString());
+        array.reverse();
+        array.insertAt(25,2);
+        assertEquals("[10,20,25,30,40]",array.toString());
     }
 
 }
