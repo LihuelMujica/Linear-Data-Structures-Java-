81edca453ba87afc4fafde205dc870a812cd09f2
LihuelMujica <lihuelmujica@gmail.com>
2022-10-08 19:13:52 -0300

    Find first non-repeated character exercise

--- hashtables/Main.java
+++ hashtables/Main.java
@@ -1,10 +1,16 @@
 package hashtables;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(firstNonRepeatedChar("a green apple"));
+    }
+
+    public void playingWithHashTables() {
         // Key: Employee Number (Integer)
         // Value: Name (String)
         Map<Integer, String> map = new HashMap<>();
@@ -28,5 +34,23 @@ public class Main {
 
         for (var item : map.entrySet())
             System.out.println(item.getValue());
+    }
+
+    public static Character firstNonRepeatedChar(String string) {
+        string = string.toLowerCase(Locale.ROOT);
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char ch : string.toCharArray()) {
+            if (!map.containsKey(ch))
+                map.put(ch,0);
+            map.put(ch, map.get(ch) + 1);
+        }
+
+        for (char ch : string.toCharArray()) {
+            if (map.get(ch) == 1) return ch;
+        }
+
+        return null;
     }
 }
